549385ac67b54b04657b16d06b1b2674277ba70d
jussarasoares <jussaracruzsoares@gmail.com>
2020-05-18 00:02:58 -0300

    copy normalize.css and app.css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Topbar from "./components/Topbar";
+
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+    </div>
+  );
+}
+
+export default App;
